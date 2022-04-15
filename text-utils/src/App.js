283cda5055f878d0353aa1bbb7274caaf05d6b3f
Pratiksha Gupta <80025856+Pratiksha102@users.noreply.github.com>
2022-04-15 19:32:56 +0530

    Basic State Functions

--- text-utils/src/App.js
+++ text-utils/src/App.js
@@ -2,29 +2,28 @@
 import './App.css';
 import About from './Components/About';
 import Navbar from './Components/Navbar';
-import { Routes  } from 'react-router-dom';
-import {
-  BrowserRouter as Router,
- 
-  Route,
-  Link
-} from "react-router-dom";
+import React, {useState} from 'react';
+
 
 function App() {
+
+  const [data,setData]=useState(null)
+  function getData(val)
+  {
+    console.log(val.target.value);
+    setData(val.target.value);
+  }
   return (
     
     <>
-  
+    <div className='App'>
+      <h2>{data}</h2>
+      <input type="text" onChange={getData}></input>
+      
+
+    </div>
     
-    <Router>
-   <Routes>
-         
-          <Route path="/about" element={<About/>} />
-          <Route path="/" element={<Navbar/>} />
-          
-          
-        </Routes>
-      </Router>  
+   
     </>
   );
 }
